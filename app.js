9d2089584f1896f66bd9fa1a30c97b457cee1897
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-07-22 18:51:18 +0100

    asyncronous basics

---
+++ app.js
@@ -0,0 +1,12 @@
+console.log('Starting...');
+
+// async function
+setTimeout(() => {
+  console.log('2 seconds Timer');
+}, 2000);
+
+setTimeout(() => {
+  console.log('0 Seconds Timer');
+}, 0);
+
+console.log('Stoping!');
