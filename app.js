4ca8a9b84874b8880b30af5b58cd4f8680c49cde
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-07-23 12:27:42 +0100

    npm init

--- app.js
+++ app.js
@@ -1,12 +1,2 @@
-console.log('Starting...');
+const request = require('postman-request');
 
-// async function
-setTimeout(() => {
-  console.log('2 seconds Timer');
-}, 2000);
-
-setTimeout(() => {
-  console.log('0 Seconds Timer');
-}, 0);
-
-console.log('Stoping!');
