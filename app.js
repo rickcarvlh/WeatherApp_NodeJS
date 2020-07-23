d531e314557fce4fa9dd44eeb1bb546a63734193
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-07-23 13:50:29 +0100

    making http requests

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
 const request = require('postman-request');
 
+// hardcoded url -> bad code
+const url =
+  'http://api.weatherstack.com/current?access_key=2bacbb8ab8fe4b5dcf92b55f1d8ba452&query=37.8267,-122,4233';
+
+request({ url: url }, (error, response) => {
+  const data = JSON.parse(response.body);
+  console.log(data.current);
+});
