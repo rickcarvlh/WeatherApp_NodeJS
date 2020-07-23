14d119e73ecea2ee83a346dd96e36377ce4b9972
ricarvlh <ricardo_carvalho84@yahoo.com>
2020-07-23 14:14:00 +0100

    customizing http request

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const request = require('postman-request');
 
 // hardcoded url -> bad code
 const url =
-  'http://api.weatherstack.com/current?access_key=2bacbb8ab8fe4b5dcf92b55f1d8ba452&query=37.8267,-122,4233';
+  'http://api.weatherstack.com/current?access_key=2bacbb8ab8fe4b5dcf92b55f1d8ba452&query=37.8267,-122,4233&units=m';
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.current);
+request({ url: url, json: true }, (error, response) => {
+  // console.log(response.body.current);
+  console.log(
+    'It is currently ' +
+      response.body.current.temperature +
+      ' degrees. It feels like ' +
+      response.body.current.feelslike +
+      ' degrees outside'
+  );
 });
